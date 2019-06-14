018d885f418d1c3ddefd304aa17cbfbd033c302d
unknown <mycool0905@gmail.com>
2019-06-14 22:48:19 +0900

    2019-06-14

--- Project/app.js
+++ Project/app.js
@@ -17,6 +17,7 @@ var expressSession = require('express-session');
 
 /* Express 객체 생성 */
 var app = express();
+var user = require('./routes/user');
 
 /* Express 객체 기본 속성 설정 */
 app.set('port',process.env.PORT || 2402);
@@ -73,7 +74,7 @@ function connectDB(){
         
         
         // user 스키마 및 모델 객체 생성
-        createUserSchema();
+        UserSchema = require('./database/user_schema').createSchema(mongoose);
         
         UserSchema.static('findById', function(id,callback){
             return this.find({id : id}, callback);
@@ -86,6 +87,8 @@ function connectDB(){
         // UserModel 모델 정의
         UserModel = mongoose.model('users', UserSchema);
         console.log('UserModel 정의함.');
+        
+        user.init(database, UserSchema, UserModel);
     });
     
     // 연결 끊어졌을 때 5초후 재연결
@@ -95,190 +98,16 @@ function connectDB(){
     });
 }
 
-/* user 스키마 및 모델 객체 생성 */
-function createUserSchema(){
-    
-    // user_schema.js 모듈 불러오기
-    UserSchema = require('./database/user_schema').createSchema(mongoose);
-}
-
-/* 사용자를 인증하는 함수 */
-var authUser = function(database, id, password, callback){
-    console.log('authUser 호출됨 : ' + id + ', ' + password);
-    
-    // 1. 아이디를 사용해 검색
-    // 모델 객체의 findById() 메소드를 호출할 때는 id 값과 콜백 함수를 전달한다.
-    // 콜백 함수에서 결과 데이터를 배열로 받으면 그 배열 객체에 데이터가 있는지 확인한다.
-    // 데이터가 있는 경우에는 첫 번째 배열 요소의 _doc 속성을 참조한다.
-    // _doc 속성은 각 문서 객체의 정보를 담고 있어 그 안에 있는 password 속성 값을 확인할 수 있다.
-    UserModel.findById(id, function(err,results){
-        if(err){
-            callback(err,null);
-            return;
-        }
-        
-        console.log('아이디 [%s]로 사용자 검색 결과', id);
-        console.dir(results);
-        
-        if(results.length>0){
-            console.log('아이디와 일치하는 사용자 찾음.');
-            
-            // 2. 비밀번호 확인
-            var user = new UserModel({id : id});
-            var authenticated = user.authenticate(password, results[0]._doc.salt, results[0]._doc.hashed_password);
-            if(authenticated){
-                console.log('비밀번호 일치함');
-                callback(null, results);
-            } else {
-                console.log('비밀번호 일치하지 않음');
-                callback(null, null);
-            }
-        } else {
-            console.log('아이디와 일치하는 사용자를 찾지 못함.');
-            callback(null,null);
-        }
-    });
-}
-
-/* 사용자를 등록하는 함수 */
-var addUser = function(database, id, password, name, callback){
-    console.log('addUser 호출됨.');
-    
-    // UserModel의 인스턴스 생성
-    var user = new UserModel({'id' : id, 'password' : password, 'name' : name});
-    
-    // save()로 저장
-    user.save(function(err){
-        if(err){
-            callback(err, null);
-            return;
-        }
-        console.log('사용자 데이터 추가함.');
-        callback(null, user);
-        
-    });
-};
-
 /* 라우터 객체 참조 */
 var router = express.Router();
 
 /* 로그인 라우팅 함수 - 데이터베이스의 정보와 비교 */
-router.route('/process/login').post(function(req,res){
-    console.log('/process/login 호출됨.');
-    
-    var paramId = req.body.id;
-    var paramPassword = req.body.password;
-    
-    if(database){
-        authUser(database, paramId, paramPassword, function(err,docs){
-            if(err) {throw err;}
-            
-            if(docs){
-                console.dir(docs);
-                var username = docs[0].name;
-                res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-                res.write('<h1>로그인 성공</h1>');
-                res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
-                res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
-                res.write('<br><br><a href="../../public/login.html">다시 로그인하기</a>');
-                res.end();
-            } else {
-                res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-                res.write('<h1>로그인 실패</h1>');
-                res.write('<div><p>아이디와 비밀번호를 다시 확인하십시오.</p></div>');
-                res.write('<br><br><a href="../../public/login.html">다시 로그인하기</a>');
-                res.end();
-            }
-        });
-    } else {
-        res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-        res.write('<h2>데이터베이스 연결 실패</h2>');
-        res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
-        res.end();
-    }
-});
+router.route('/process/login').post(user.login);
 
-router.route('/process/adduser').post(function(req, res){
-    console.log('/process/adduser 호출됨.');
-    
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-    var paramName = req.body.name || req.query.name;
-    
-    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
-    
-    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
-    if(database) {
-        addUser(database, paramId, paramPassword, paramName, function(err,result){
-            if(err) {throw err;}
-            
-            // 결과 객체 확인하여 추가된 데이터 있으면 성공 응답 전송
-            if(result && result.insertedCount > 0){
-                console.dir(result);
-                
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8}'});
-                res.write('<h2>사용자 추가 성공</h2>');
-                res.end();
-            } else { // 결과 객체가 없으면 실패 응답 전송
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8}'});
-                res.write('<h2>사용자 추가 실패</h2>');
-                res.end();
-            }
-        });
-    } else { // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-        res.writeHead('200', {'Content-Type':'text/html;charset=utf8}'});
-        res.write('<h2>데이터베이스 연결 실패</h2>');
-        res.end();
-    }
-});
+router.route('/process/adduser').post(user.adduser);
 
 /* 사용자 리스트 함수 */
-router.route('/process/listuser').post(function(req,res){
-    console.log('/process/listuser 호출됨.');
-    
-    // 데이터베이스 객체가 초기화된 경우, 모델 객체의 findAll 메소드 호출
-    if (database){
-        // 1. 모든 사용자 검색
-        UserModel.findAll(function(err,results){
-            // 에러가 발생했을 때 클라이언트로 오류 전송
-            if(err){
-                console.error('사용자 리스트 조회 중 오류 발생 : ' + err.stack);
-                
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-                res.write('<h2>사용자 리스트 조회 중 오류 발생</h2>');
-                res.write('<p>' + err.stack + '</p>');
-                res.end();
-                
-                return;
-            }
-            
-            if (results){ // 결과 객체 있으면 리스트 전송
-                console.dir(results);
-                
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-                res.write('<h2>사용자 리스트</h2>');
-                res.write('<div><ul>');
-                
-                for(var i = 0; i < results.length; i++){
-                    var curId = results[i]._doc.id;
-                    var curName = results[i]._doc.name;
-                    res.write('   <li>#' + i + ' : ' + curId + ', ' + curName + '</li>');
-                }
-                
-                res.write('</ul></div>');
-                res.end();
-            } else { // 결과 객체 없으면 실패 응답 전송
-                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-                res.write('<h2>사용자 리스트 조회 실패</h2>');
-                res.end();
-            }
-        });
-    } else { // 데이터베이스 객체가 초기화되지 않았을 때 실패 응답 전송
-        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-        res.write('<h2>데이터베이스 연결 실패</h2>');
-        res.end();
-    }
-});
+router.route('/process/listuser').post(user.listuser);
 
 /* 라우터 객체 등록 */
 app.use('/',router);
