74409f947a9f29910aa2c96ccc4df8ce344257b9
unknown <mycool0905@gmail.com>
2019-06-16 00:34:58 +0900

    2019-06-16-00:34

--- Project/app.js
+++ Project/app.js
@@ -18,6 +18,7 @@ var expressSession = require('express-session');
 /* Passport 사용 */
 var passport = require('passport');
 var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
 
 /* Express 객체 생성 */
 var app = express();
@@ -98,8 +99,144 @@ function connectDB(){
     });
 }
 
+/* 패스포트 로그인 설정 */
+passport.use('local-login', new LocalStrategy({
+    usernameField : 'id',
+    passwordField : 'password',
+    passReqToCallback : true
+}, function(req, id, password, done){
+    console.log('passport의 local-login 호출됨 : ' + id + ', ' + password);
+    
+    UserModel.findOne({'id' : id}, function(err, member){
+        if(err) {return done(err);}
+        
+        // 등록된 사용자가 없는 경우
+        if(!member){
+            console.log('계정이 일치하지 않음.');
+            return done(null, false, req.flash('loginMessage', '등록된 계정이 없습니다.'));
+        }
+        
+        // 비밀번호를 비교하여 맞지 않는 경우
+        var authenticated = member.authenticate(password, member._doc.salt, member._doc.hashed_password);
+        if(!authenticated) {
+            console.log('비밀번호 일치하지 않음.');
+            return done(null, false, req.flash('loginMessage', '비밀번호가 일치하지 않습니다.'));
+        }
+        
+        // 정상인 경우
+        console.log('계정과 비밀번호가 일치함.');
+        return done(null, member);
+    });
+}));
+
+/* 패스포트 회원가입 설정 */
+passport.use('local-signup', new LocalStrategy({
+    usernameField : 'id',
+    passwordField : 'password',
+    passReqToCallback : true
+}, function(req, id, password, done){
+    // 요청 파라미터 중 name 파라미터 확인
+    var paramName = req.body.name || req.query.name;
+    console.log('passport의 local-signup 호출됨 : ' + id + ', ' + password + ', ' + paramName);
+    
+    // User.findOne이 blocking되므로 async 방식으로 변경할 수도 있음
+    process.nextTick(function(){
+        UserModel.findOne({'id' : id}, function(err, member){
+            // 오류가 발생하면
+            if(err){
+                return done(err);
+            }
+            
+            // 기존에 이메일이 있다면
+            if(member){
+                console.log('기존에 계정이 있음.');
+                return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다.'));
+            } else {
+                // 모델 인스턴스 객체 만들어 저장
+                var member = new UserModel({'id' : id, 'password' : password, 'name' : paramName});
+                member.save(function(err){
+                    if(err) {throw err;}
+                    console.log('사용자 데이터 추가함.');
+                    return done(null, member);
+                });
+            }
+        });
+    });
+}));
+
+/* 사용자 인증에 성공했을 때 호출 */
+passport.serializeUser(function(member, done){
+    console.log('serializeUser() 호출됨.');
+    
+    done(null, member);
+});
+
+/* 사용자 인증 이후 사용자 요청이 있을 때마다 호출 */
+passport.deserializeUser(function(member, done){
+    console.log('deserializeUser() 호출됨.');
+    
+    done(null, member);
+});
+
 /* 라우터 객체 참조 */
 var router = express.Router();
+
+/* 홈 페이지 */
+router.route('/').get(function(req, res){
+    console.log('/ 패스 요청됨.');
+    res.render('index.ejs');
+});
+
+/* 로그인 폼 링크 */
+app.get('/login', function(req, res){
+    console.log('/login 패스 요청됨.');
+    res.render('login.ejs', {message : req.flash('loginMessage')});
+});
+
+app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile',
+    failureRedirect : '/login',
+    failureFlash : true
+}));
+
+/* 회원가입 폼 링크 */
+app.get('/signup', function(req, res){
+    console.log('/signup 패스 요청됨.');
+    res.render('signup.ejs', {message : req.flash('signupMessage')});
+});
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/login',
+    failureRedirect : '/signup',
+    failureFlash : true
+}));
+
+/* 프로필 화면 - 로그인 여부를 확인할 수 있도록 먼저 isLoggedIn 미들웨어 실행 */
+router.route('/profile').get(function(req, res){
+    console.log('/profile 패스 요청됨.');
+    
+    // 인증이 안 된 경우
+    if(!req.user){
+        console.log('사용자 인증이 안 된 상태임.');
+        res.redirect('/');
+        return;
+    }
+    
+    // 인증된 경우
+    console.log('사용자 인증된 상태임.');
+    if(Array.isArray(req.user)){
+        res.render('profile.ejs', {user: req.user[0]._doc});
+    } else {
+        res.render('profile.ejs', {user: req.user});
+    }
+});
+
+/* 로그아웃 */
+app.get('/logout', function(req, res){
+    console.log('/logout 패스 요청됨.');
+    req.logout();
+    res.redirect('/');
+});
 
 /* 로그인 라우팅 함수 - 데이터베이스의 정보와 비교 */
 router.route('/process/login').post(user.login);
